a95883cc34390f5d61c741822c1a4d4c2cd8f532
PrestonFr <PrestonFr@gmail.com>
2022-08-17 20:33:50 -0400

    update

--- public/index.js
+++ public/index.js
@@ -1,4 +1,13 @@
 function main(){
+    let response = await fetch("http://127.0.0.1:5500/public/")
+        
+    let resultObject = await response.text()
+        
+    console.log(resultObject)
+
+        
+
+
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
